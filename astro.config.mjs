a68bcb6e0edb073fcfd1dc4407a989d1d5642bf7
5haeq <hectoryampufebv@gmail.com>
2025-12-09 19:03:25 -0500

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://trikaweb.com',
   output: 'server',
   adapter: vercel(),
   vite: {
