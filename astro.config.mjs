c5fece1c4120abc4130de288a65029ff84aeeb62
Alexis Garay <alexis.garay.y@uni.pe>
2025-12-10 20:11:48 -0500

    SEO: se migró del dominio de trika-web a trikaweb

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://trika-web.vercel.app',
+  site: 'https://trikaweb.vercel.app',
   output: 'server',
   adapter: vercel(),
   vite: {
